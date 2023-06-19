24d2d79c149e44eda59e89cccf442e2ff7e49d07
Cleito Karloh <cleito.kp@gmail.com>
2023-06-19 16:57:20 -0300

    Add wallet connection and resources to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,48 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
         </h1>
-          <Link href="/page2" className={styles.card}>
+
+        <p className={styles.description}>
+          Get started by configuring your desired network in{" "}
+          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
+          <code className={styles.code}>pages/index.tsx</code> file!
+        </p>
+
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+
+        <Link href="/page2" className={styles.card}>
             <h2>Teste aqui</h2>
           </Link>
+
+        <div className={styles.grid}>
+          <a href="https://portal.thirdweb.com/" className={styles.card}>
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className={styles.card}>
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a
+            href="https://portal.thirdweb.com/templates"
+            className={styles.card}
+          >
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
       </main>
     </div>
   );
